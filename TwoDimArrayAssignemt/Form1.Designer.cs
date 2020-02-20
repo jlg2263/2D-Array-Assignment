f4308d5dde997766c5cc1054844f6b7284422063
Jared Gain <jlgain7@gmail.com>
2020-02-20 08:44:48 -0600

    Updated Files

---
+++ TwoDimArrayAssignemt/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace TwoDimArrayAssignemt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numOfStudentsLabel = new System.Windows.Forms.Label();
+            this.numOfExamsLabel = new System.Windows.Forms.Label();
+            this.numOfStudentsTextbox = new System.Windows.Forms.TextBox();
+            this.numOfExamsTextbox = new System.Windows.Forms.TextBox();
+            this.numOfStudentsButton = new System.Windows.Forms.Button();
+            this.numOfExamsButton = new System.Windows.Forms.Button();
+            this.classGroupBox = new System.Windows.Forms.GroupBox();
+            this.scoreButton = new System.Windows.Forms.Button();
+            this.nameButton = new System.Windows.Forms.Button();
+            this.examScoreTextbox = new System.Windows.Forms.TextBox();
+            this.examScoreLabel = new System.Windows.Forms.Label();
+            this.studentNameTextbox = new System.Windows.Forms.TextBox();
+            this.studentNameLabel = new System.Windows.Forms.Label();
+            this.classResultsButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.classGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // numOfStudentsLabel
+            // 
+            this.numOfStudentsLabel.AutoSize = true;
+            this.numOfStudentsLabel.Location = new System.Drawing.Point(13, 25);
+            this.numOfStudentsLabel.Name = "numOfStudentsLabel";
+            this.numOfStudentsLabel.Size = new System.Drawing.Size(132, 13);
+            this.numOfStudentsLabel.TabIndex = 7;
+            this.numOfStudentsLabel.Text = "Enter Number of Students:";
+            // 
+            // numOfExamsLabel
+            // 
+            this.numOfExamsLabel.AutoSize = true;
+            this.numOfExamsLabel.Location = new System.Drawing.Point(24, 66);
+            this.numOfExamsLabel.Name = "numOfExamsLabel";
+            this.numOfExamsLabel.Size = new System.Drawing.Size(121, 13);
+            this.numOfExamsLabel.TabIndex = 8;
+            this.numOfExamsLabel.Text = "Enter Number of Exams:";
+            // 
+            // numOfStudentsTextbox
+            // 
+            this.numOfStudentsTextbox.Location = new System.Drawing.Point(152, 17);
+            this.numOfStudentsTextbox.Name = "numOfStudentsTextbox";
+            this.numOfStudentsTextbox.Size = new System.Drawing.Size(100, 20);
+            this.numOfStudentsTextbox.TabIndex = 0;
+            // 
+            // numOfExamsTextbox
+            // 
+            this.numOfExamsTextbox.Location = new System.Drawing.Point(152, 59);
+            this.numOfExamsTextbox.Name = "numOfExamsTextbox";
+            this.numOfExamsTextbox.Size = new System.Drawing.Size(100, 20);
+            this.numOfExamsTextbox.TabIndex = 2;
+            // 
+            // numOfStudentsButton
+            // 
+            this.numOfStudentsButton.Location = new System.Drawing.Point(259, 13);
+            this.numOfStudentsButton.Name = "numOfStudentsButton";
+            this.numOfStudentsButton.Size = new System.Drawing.Size(126, 23);
+            this.numOfStudentsButton.TabIndex = 1;
+            this.numOfStudentsButton.Text = "Number of Students";
+            this.numOfStudentsButton.UseVisualStyleBackColor = true;
+            this.numOfStudentsButton.Click += new System.EventHandler(this.numOfStudentsButton_Click);
+            // 
+            // numOfExamsButton
+            // 
+            this.numOfExamsButton.Location = new System.Drawing.Point(259, 56);
+            this.numOfExamsButton.Name = "numOfExamsButton";
+            this.numOfExamsButton.Size = new System.Drawing.Size(126, 23);
+            this.numOfExamsButton.TabIndex = 3;
+            this.numOfExamsButton.Text = "Number of Exams";
+            this.numOfExamsButton.UseVisualStyleBackColor = true;
+            this.numOfExamsButton.Click += new System.EventHandler(this.numOfExamsButton_Click);
+            // 
+            // classGroupBox
+            // 
+            this.classGroupBox.Controls.Add(this.scoreButton);
+            this.classGroupBox.Controls.Add(this.nameButton);
+            this.classGroupBox.Controls.Add(this.examScoreTextbox);
+            this.classGroupBox.Controls.Add(this.examScoreLabel);
+            this.classGroupBox.Controls.Add(this.studentNameTextbox);
+            this.classGroupBox.Controls.Add(this.studentNameLabel);
+            this.classGroupBox.Location = new System.Drawing.Point(12, 112);
+            this.classGroupBox.Name = "classGroupBox";
+            this.classGroupBox.Size = new System.Drawing.Size(373, 100);
+            this.classGroupBox.TabIndex = 4;
+            this.classGroupBox.TabStop = false;
+            this.classGroupBox.Text = "Enter Student Name and Scores";
+            // 
+            // scoreButton
+            // 
+            this.scoreButton.Location = new System.Drawing.Point(290, 54);
+            this.scoreButton.Name = "scoreButton";
+            this.scoreButton.Size = new System.Drawing.Size(75, 23);
+            this.scoreButton.TabIndex = 3;
+            this.scoreButton.Text = "Score";
+            this.scoreButton.UseVisualStyleBackColor = true;
+            this.scoreButton.Click += new System.EventHandler(this.scoreButton_Click);
+            // 
+            // nameButton
+            // 
+            this.nameButton.Location = new System.Drawing.Point(290, 18);
+            this.nameButton.Name = "nameButton";
+            this.nameButton.Size = new System.Drawing.Size(75, 23);
+            this.nameButton.TabIndex = 1;
+            this.nameButton.Text = "Name";
+            this.nameButton.UseVisualStyleBackColor = true;
+            this.nameButton.Click += new System.EventHandler(this.nameButton_Click);
+            // 
+            // examScoreTextbox
+            // 
+            this.examScoreTextbox.Location = new System.Drawing.Point(140, 57);
+            this.examScoreTextbox.Name = "examScoreTextbox";
+            this.examScoreTextbox.Size = new System.Drawing.Size(100, 20);
+            this.examScoreTextbox.TabIndex = 2;
+            // 
+            // examScoreLabel
+            // 
+            this.examScoreLabel.AutoSize = true;
+            this.examScoreLabel.Location = new System.Drawing.Point(18, 65);
+            this.examScoreLabel.Name = "examScoreLabel";
+            this.examScoreLabel.Size = new System.Drawing.Size(95, 13);
+            this.examScoreLabel.TabIndex = 5;
+            this.examScoreLabel.Text = "Enter Exam Score:";
+            // 
+            // studentNameTextbox
+            // 
+            this.studentNameTextbox.Location = new System.Drawing.Point(140, 22);
+            this.studentNameTextbox.Name = "studentNameTextbox";
+            this.studentNameTextbox.Size = new System.Drawing.Size(143, 20);
+            this.studentNameTextbox.TabIndex = 0;
+            // 
+            // studentNameLabel
+            // 
+            this.studentNameLabel.AutoSize = true;
+            this.studentNameLabel.Location = new System.Drawing.Point(15, 30);
+            this.studentNameLabel.Name = "studentNameLabel";
+            this.studentNameLabel.Size = new System.Drawing.Size(106, 13);
+            this.studentNameLabel.TabIndex = 4;
+            this.studentNameLabel.Text = "Enter Student Name:";
+            // 
+            // classResultsButton
+            // 
+            this.classResultsButton.Location = new System.Drawing.Point(50, 245);
+            this.classResultsButton.Name = "classResultsButton";
+            this.classResultsButton.Size = new System.Drawing.Size(75, 37);
+            this.classResultsButton.TabIndex = 5;
+            this.classResultsButton.Text = "Show Class Results";
+            this.classResultsButton.UseVisualStyleBackColor = true;
+            this.classResultsButton.Visible = false;
+            this.classResultsButton.Click += new System.EventHandler(this.classResultsButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.outputLabel.Location = new System.Drawing.Point(152, 215);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(143, 114);
+            this.outputLabel.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 338);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.classResultsButton);
+            this.Controls.Add(this.classGroupBox);
+            this.Controls.Add(this.numOfExamsButton);
+            this.Controls.Add(this.numOfStudentsButton);
+            this.Controls.Add(this.numOfExamsTextbox);
+            this.Controls.Add(this.numOfStudentsTextbox);
+            this.Controls.Add(this.numOfExamsLabel);
+            this.Controls.Add(this.numOfStudentsLabel);
+            this.Name = "Form1";
+            this.Text = "Enter Names and Scores for Exams";
+            this.classGroupBox.ResumeLayout(false);
+            this.classGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numOfStudentsLabel;
+        private System.Windows.Forms.Label numOfExamsLabel;
+        private System.Windows.Forms.TextBox numOfStudentsTextbox;
+        private System.Windows.Forms.TextBox numOfExamsTextbox;
+        private System.Windows.Forms.Button numOfStudentsButton;
+        private System.Windows.Forms.Button numOfExamsButton;
+        private System.Windows.Forms.GroupBox classGroupBox;
+        private System.Windows.Forms.Button scoreButton;
+        private System.Windows.Forms.Button nameButton;
+        private System.Windows.Forms.TextBox examScoreTextbox;
+        private System.Windows.Forms.Label examScoreLabel;
+        private System.Windows.Forms.TextBox studentNameTextbox;
+        private System.Windows.Forms.Label studentNameLabel;
+        private System.Windows.Forms.Button classResultsButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ TwoDimArrayAssignemt/Form1.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Jared Gain
+ * CIS 466
+ * 10/2/2017
+ * This project displays the class results after entering
+ * a certain amount of students and their exam scores
+ */
+namespace TwoDimArrayAssignemt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Delcare field variables
+        private int numOfStudents = 0;
+        private int numOfExams = 0;
+        private string studentName = "";
+        private int[,] studentScores = new int[0, 0];
+        private string[] studentNames = new string[0];
+        private int nameCounter = 0;
+        private int rowCounter = 0;
+        private int colCounter = 0;
+
+        private void numOfStudentsButton_Click(object sender, EventArgs e)
+        {
+            // Try-Catch statement
+            try
+            {
+                if (IsStudentDataValid())
+                {
+                    // Convert data
+                    numOfStudents = Convert.ToInt32(numOfStudentsTextbox.Text);
+
+                    // Hide or disable appropriate objects on form
+                    numOfStudentsLabel.Visible = false;
+                    numOfStudentsTextbox.Visible = false;
+                    numOfStudentsButton.Visible = false;
+                    numOfExamsTextbox.Focus();
+                }
+            }
+            catch (FormatException error)
+            {
+                MessageBox.Show(error.Message + "\n\n" + error.GetType().ToString() + "\n\n"
+                                + error.StackTrace, "Entry Error");
+                numOfStudentsTextbox.Focus();
+            }
+            catch (OverflowException error)
+            {
+                MessageBox.Show(error.Message + "\n\n" + error.GetType().ToString() + "\n\n"
+                                + error.StackTrace, "Entry Error");
+                numOfStudentsTextbox.Focus();
+            }
+            catch (DivideByZeroException error)
+            {
+                MessageBox.Show(error.Message + "\n\n" + error.GetType().ToString() + "\n\n"
+                                + error.StackTrace, "Entry Error");
+                numOfStudentsTextbox.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n\n" + ex.GetType().ToString() + "\n\n"
+                                + ex.StackTrace, "Entry Error");
+                numOfStudentsTextbox.Focus();
+            }
+        }
+
+        private void numOfExamsButton_Click(object sender, EventArgs e)
+        {
+            // Try-Catch statement
+            try
+            {
+                if (IsExamDataValid())
+                {
+                    // Convert data
+                    numOfExams = Convert.ToInt32(numOfExamsTextbox.Text);
+
+                    // Initialize a 2D int array
+                    studentScores = new int[numOfStudents, numOfExams];
+
+                    // Initialize new value for array
+                    studentNames = new string[numOfStudents];
+
+                    // Hide or disable appropriate objects on form
+                    numOfExamsLabel.Visible = false;
+                    numOfExamsTextbox.Visible = false;
+                    numOfExamsButton.Visible = false;
+                    studentNameTextbox.Focus();
+                }
+            }
+            catch (FormatException error)
+            {
+                MessageBox.Show(error.Message + "\n\n" + error.GetType().ToString() + "\n\n"
+                                + error.StackTrace, "Entry Error");
+                numOfStudentsTextbox.Focus();
+            }
+            catch (OverflowException error)
+            {
+                MessageBox.Show(error.Message + "\n\n" + error.GetType().ToString() + "\n\n"
+                                + error.StackTrace, "Entry Error");
+                numOfStudentsTextbox.Focus();
+            }
+            catch (DivideByZeroException error)
+            {
+                MessageBox.Show(error.Message + "\n\n" + error.GetType().ToString() + "\n\n"
+                                + error.StackTrace, "Entry Error");
+                numOfStudentsTextbox.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n\n" + ex.GetType().ToString() + "\n\n"
+                                + ex.StackTrace, "Entry Error");
+                numOfStudentsTextbox.Focus();
+            }
+        }
+
+        private void nameButton_Click(object sender, EventArgs e)
+        {
+            // Try-Catch statement
+            try
+            {
+                if (IsNameDataValid())
+                {
+                    if (nameCounter < numOfStudents)
+                    {
+                        // Add elements to array
+                        studentNames[nameCounter] = studentNameTextbox.Text;
+
+                        // Increment counter
+                        nameCounter++;
+
+                        // Hide or disable appropriate objects on form
+                        studentNameTextbox.Enabled = false;
+                        studentNameLabel.Visible = false;
+                        nameButton.Visible = false;
+                        examScoreTextbox.Enabled = true;
+                        examScoreTextbox.Focus();
+                        scoreButton.Enabled = true;
+                    }
+                }
+            }
+            catch (FormatException error)
+            {
+                MessageBox.Show(error.Message + "\n\n" + error.GetType().ToString() + "\n\n"
+                                + error.StackTrace, "Entry Error");
+                numOfStudentsTextbox.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n\n" + ex.GetType().ToString() + "\n\n"
+                                + ex.StackTrace, "Entry Error");
+                numOfStudentsTextbox.Focus();
+            }
+        }
+
+        private void scoreButton_Click(object sender, EventArgs e)
+        {
+            // Try-Catch statement
+            try
+            {
+                if (IsScoreDataValid())
+                {
+                    if (colCounter < numOfExams)
+                    {
+                        studentScores[rowCounter, colCounter] = int.Parse(examScoreTextbox.Text);
+
+                        // Increment column counter
+                        colCounter++;
+
+                        // Clear textbox
+                        examScoreTextbox.Text = "";
+                        examScoreTextbox.Focus();
+
+                        if (colCounter == numOfExams)
+                        {
+                            MessageBox.Show("All Scores have been entered for " + studentNames[rowCounter] +
+                                            ".", "Student Update");
+                            // Increment row counter
+                            rowCounter++;
+
+                            // Set column counter back to zero
+                            colCounter = 0;
+
+                            // Hide or disable appropriate objects on form
+                            studentNameLabel.Visible = true;
+                            nameButton.Visible = true;
+                            studentNameTextbox.Enabled = true;
+                            studentNameTextbox.Text = "";
+                            studentNameTextbox.Focus();
+                            examScoreTextbox.Enabled = false;
+                            scoreButton.Enabled = false;
+                        }
+                    }
+                    
+                    if (rowCounter == numOfStudents)
+                    {
+                        classGroupBox.Visible = false;
+                        classResultsButton.Visible = true;
+                        MessageBox.Show("All Student Scores have been entered. Please Press Show " +
+                                       "Results Button.", "Student Update");
+                    }
+                }
+            }
+            catch (FormatException error)
+            {
+                MessageBox.Show(error.Message + "\n\n" + error.GetType().ToString() + "\n\n"
+                                + error.StackTrace, "Entry Error");
+                numOfStudentsTextbox.Focus();
+            }
+            catch (OverflowException error)
+            {
+                MessageBox.Show(error.Message + "\n\n" + error.GetType().ToString() + "\n\n"
+                                + error.StackTrace, "Entry Error");
+                numOfStudentsTextbox.Focus();
+            }
+            catch (DivideByZeroException error)
+            {
+                MessageBox.Show(error.Message + "\n\n" + error.GetType().ToString() + "\n\n"
+                                + error.StackTrace, "Entry Error");
+                numOfStudentsTextbox.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n\n" + ex.GetType().ToString() + "\n\n"
+                                + ex.StackTrace, "Entry Error");
+                numOfStudentsTextbox.Focus();
+            }
+        }
+
+        private void classResultsButton_Click(object sender, EventArgs e)
+        {
+            // Declare and initialize local string variables
+            string output = "";
+            int sum = 0;
+            int average = 0;
+
+            // Nest For Loop
+            for (int row = 0; row < studentScores.GetLength(0); row++)
+            {
+                output = output + studentNames[row] + " ";
+
+                for (int col = 0; col < studentScores.GetLength(1); col++)
+                {
+                    // Traverse through array
+                    output = output + studentScores[row, col] + " ";
+
+                    // Calculate sum of row
+                    sum = sum + studentScores[row, col];
+                }
+
+                // Calculate average
+                average = sum / numOfExams;
+
+                output = output + average.ToString("n2") + "\n";
+
+                //Set sum and average to zero
+                sum = 0;
+                average = 0;
+            }
+
+            // Display Results
+            outputLabel.Text = output;
+        }
+
+        // IsStudentDataValid method
+        private bool IsStudentDataValid()
+        {
+            return
+
+            IsPresent(numOfStudentsTextbox, "Number of Students") &&
+            IsInteger(numOfStudentsTextbox, "Number of Students") &&
+            IsGreaterThanZero(numOfStudentsTextbox, "Number of Students");
+        }
+
+        // IsExamDataValid method
+        private bool IsExamDataValid()
+        {
+            return
+
+            IsPresent(numOfExamsTextbox, "Number of Exams") &&
+            IsInteger(numOfExamsTextbox, "Number of Exams") &&
+            IsGreaterThanZero(numOfExamsTextbox, "Number of Exams");
+        }
+
+        // IsNameDataValid method
+        private bool IsNameDataValid()
+        {
+            return
+
+            IsPresent(studentNameTextbox, "Student Name");
+        }
+
+        // IsScoreDataValid method
+        private bool IsScoreDataValid()
+        {
+            return
+
+            IsPresent(examScoreTextbox, "Student Score") &&
+            IsInteger(examScoreTextbox, "Student Score") &&
+            IsGreaterThanZero(examScoreTextbox, "Student Score");
+        }
+
+        // IsPresent method
+        public bool IsPresent(TextBox textbox, string name)
+        {
+            // Set flag variable to true
+            bool value = true;
+
+            if (textbox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field. Please enter a valid data type.", "Entry Error");
+                textbox.Focus();
+                textbox.ForeColor = Color.Red;
+                value = false;
+            }
+            return value;
+        }
+
+        // IsInteger method
+        public bool IsInteger(TextBox textbox, string name)
+        {
+            // Declare and initialize variable
+            int number = 0;
+
+            // Set flag variable to true
+            bool value = true;
+
+            if (!(Int32.TryParse(textbox.Text, out number)))
+            {
+                MessageBox.Show(name + " must be an integer. Please enter a valid data type.", "Entry Error");
+                textbox.Focus();
+                textbox.ForeColor = Color.Red;
+                value = false;
+            }
+            return value;
+        }
+
+        // IsGreaterThanZero method
+        public bool IsGreaterThanZero(TextBox textbox, string name)
+        {
+            // Declare and initialize variable
+            int number = Convert.ToInt32(textbox.Text);
+
+            // Set flag variable to true
+            bool value = true;
+
+            if (number <= 0)
+            {
+                MessageBox.Show(name + " must be greater than 0. Please enter valid data.", "Entry Error");
+                textbox.Focus();
+                textbox.ForeColor = Color.Red;
+                value = false;
+            }
+            return value;
+        }
+    }
+}
